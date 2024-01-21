e56776bad9d6ec1f1466d9e14ebe4edcd2727069
John Okyere <mhiskall282@users.noreply.github.com>
2024-01-21 23:19:21 +0000

    *

--- script.js
+++ script.js
@@ -8,42 +8,31 @@ function toggleMenu() {
 
 // Form Submission
 document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("messageForm").addEventListener("submit", function (event) {
-    // Your existing form submission code here
+  const messageForm = document.getElementById("messageForm");
 
-    // Example: You can prevent the default form submission
-    event.preventDefault();
+  messageForm.addEventListener("submit", function (event) {
+    // Example: You can prevent the default form submission only if it's not the hamburger menu button
+    if (!event.target.querySelector('.hamburger-icon').contains(document.activeElement)) {
+      event.preventDefault();
 
-    // Add your logic to send the message to your server
-    // For simplicity, we'll just log a message to the console
-    console.log("Message submitted!");
+      // Add your logic to send the message to your server
+      // For simplicity, we'll just log a message to the console
+      console.log("Message submitted!");
+    }
   });
 });
 
 // Service Worker
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
-})
+});
 
 async function handleRequest(request) {
   const response = await fetch('https://github.com/mhiskall282/portfoliowebsite/blob/main/index.html', {
     cf: {
       cacheTtl: 86400, // Cache for 24 hours
     },
-  })
+  });
 
-  return response
+  return response;
 }
-
-document.getElementById("messageForm").addEventListener("submit", function (event) {
-  // Your existing form submission code here
-
-  // Example: You can prevent the default form submission only if it's not the hamburger menu button
-  if (!event.target.querySelector('.hamburger-icon').contains(event.target.activeElement)) {
-      event.preventDefault();
-
-      // Add your logic to send the message to your server
-      // For simplicity, we'll just log a message to the console
-      console.log("Message submitted!");
-  }
-});
