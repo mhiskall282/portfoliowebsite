1cd1a29c08beed6d1c0901c41b92409001bad50d
John Okyere <mhiskall282@users.noreply.github.com>
2024-01-21 22:23:11 +0000

    work

--- script.js
+++ script.js
@@ -14,8 +14,6 @@ document.getElementById('messageForm').addEventListener('submit', function(event
     // You can add your own logic here, like sending the message to your server
 
     // Display a confirmation message (you can replace this with your own logic)
-    alert('Message sent successfully!');
-    // Alternatively, you can redirect the user to a thank you page or do any other action.
 });
 
 
